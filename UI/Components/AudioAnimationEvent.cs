3f442c43563a04b9890f7e4a1cc2169a688de9ef
Dario D. Müller <dario-domi@hotmail.de>
2017-02-03 17:46:36 +0100

    ADDED AudioAnimationEvent

---
+++ UI/Components/AudioAnimationEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using TinyRoar.Framework;
+
+public class AudioAnimationEvent : MonoBehaviour
+{
+    public void PlayAudio(string clip)
+    {
+        SoundManager.Instance.Play(clip, SoundManager.SoundType.Soundeffect);
+    }
+}
