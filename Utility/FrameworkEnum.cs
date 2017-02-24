87dcfbade578184fe0531815e336647046ebfcc6
Dario D. Müller <dario-domi@hotmail.de>
2017-02-24 19:39:33 +0100

    ADDED framework enum

---
+++ Utility/FrameworkEnum.cs
@@ -0,0 +1,16 @@
+public enum UIAction
+{
+    None,
+    Show,
+    Hide,
+    Toggle,
+}
+
+public enum Direction
+{
+    None = -2,
+    Left = -1,
+    Right = 1,
+    Top = 0,
+    Bottom = 2,
+}
