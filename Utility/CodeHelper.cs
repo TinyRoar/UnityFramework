b2fb50eca100bda57baa5d55778fae25101f3275
Dario D. Müller <dario-domi@hotmail.de>
2017-03-23 13:38:11 +0100

    ADDED CodeHelper FindRecursive method

--- Utility/CodeHelper.cs
+++ Utility/CodeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace TinyRoar.Framework
 {
@@ -19,6 +20,25 @@ namespace TinyRoar.Framework
         public static T CreateInstance<T>(Type t)
         {
             return (T)Activator.CreateInstance(t);
+        }
+
+        /// <summary>
+        /// if you need to look recursion into deeper childs
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static Transform FindRecursive(string name, Transform t)
+        {
+            if (t.name == name)
+                return t;
+            foreach (Transform child in t)
+            {
+                var ct = FindRecursive(name, child);
+                if (ct != null)
+                    return ct;
+            }
+            return null;
         }
 
     }
