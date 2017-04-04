7c4c9731cbb22a0b196f5af6ba0fc922ccc6b6ae
Sebastian Sacher <bastisacher@freenet.de>
2017-04-04 10:59:28 +0200

    ADDED delete savegame to TinyRoar menu!

---
+++ Editor/DeleteSavegame.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using TinyRoar.Framework;
+using UnityEditor;
+using UnityEngine;
+
+public class DeleteSavegame : EditorWindow
+{
+    static string environmentsEnums = "";
+    static string layerEnums = "";
+
+    [MenuItem("Tiny Roar/Delete Savegame")]
+    public static void ShowWindow()
+    {
+        DataManagement.Instance.Reset();
+        PlayerPrefs.DeleteAll();
+        Print.Log("Savegame deleted!");
+    }
+}
