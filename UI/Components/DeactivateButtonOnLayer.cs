bf6bad736d3a9bb7f04c7df1f8b1c117c24839e1
Sebastian Sacher <bastisacher@freenet.de>
2017-06-23 15:59:44 +0200

    ADDED deactivate button if layer shown

---
+++ UI/Components/DeactivateButtonOnLayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TinyRoar.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeactivateButtonOnLayer : MonoBehaviour {
+
+    public Layer DeactivateOnLayer;
+    private Button btn;
+
+	void Start ()
+    {
+        btn = GetComponent<Button>();
+        Events.Instance.OnLayerChange += LayerChange;
+	}
+
+    private void LayerChange(Layer layer, UIAction action)
+    {
+        if (layer == DeactivateOnLayer)
+        {
+            if (action == UIAction.Show)
+                btn.interactable = false;
+
+            if (action == UIAction.Hide)
+                btn.interactable = true;
+        }
+    }
+}
