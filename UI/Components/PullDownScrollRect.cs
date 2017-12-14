3ed575de1f4ac4fbf25fa4195a6d95b1a322a248
Sebastian Sacher <bastisacher@freenet.de>
2017-12-14 12:05:49 +0100

    ADDED script wich fires event on scrollrect(elastic) pull down

---
+++ UI/Components/PullDownScrollRect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PullDownScrollRect : MonoBehaviour, IEndDragHandler
+{
+
+    public UnityEvent OnPullDown;
+    public float TriggerValue = -200;
+
+    private RectTransform contentRectTransform;
+
+    public void Start()
+    {
+        contentRectTransform = GetComponent<ScrollRect>().content.GetComponent<RectTransform>();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (TriggerValue < contentRectTransform.anchoredPosition.y)
+            return;
+
+        OnPullDown.Invoke();
+        print("Fire update event");
+    }
+}
